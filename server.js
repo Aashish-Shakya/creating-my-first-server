9452295ca6f3c5d906f2170252724e182f4ac9e1
Aashish-Shakya <bhagwandasshakya62@gmail.com>
2022-08-20 20:39:11 +0530

    installed third party package(nodemon)

--- server.js
+++ server.js
@@ -2,19 +2,34 @@ const http = require('http');
 
 const port = 8081;
 
-http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Tyoe': 'text/html' });
-    response.write("<h1>Hello, This is from my server</h1>");
-    response.end();
+http
+    .createServer((request, response) => {
+        response.writeHead(200, { 'Content-Type': 'text/html' }); // this line use send success msg by using 200 
+        // response.write("<h1>Hello All, This is from my server</h1>");
+        response.write(html);
 
-}).listen(port, () => {
-    console.log(`Nodejs server started on port ${port}`)
-})
+        response.end();
+
+    }).listen(port, () => {
+        console.log(`Nodejs server started on port ${port}`)
+    })
 
 // http://localhost:8081
 
 //we can create a shortcut for typing node server.js in package.json in script tag.
 
 //now we create shortcut which is node start/ npm start for node server.js
+// "start": "node server.js",
 // If we use a default name start then use npm start
 // If u give any name like "runThis" to start --> then u have to say npm run runThis
+
+
+
+//---> Now we are going to install nodemeon to refresh ur server/web page again angain.
+//command --> npm i nodemon
+
+//Now we are creating a command in json to restart our server agin and again
+//  "dev": "nodemon server.js"
+
+
+const html = "<h1>Hello All, This is from my server</h1><h2> This is just a simple server which i created</h2>"
