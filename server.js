ef09121b696345c448d56e7604bc67eae3d8ad98
Aashish-Shakya <bhagwandasshakya62@gmail.com>
2022-08-25 17:37:26 +0530

    "Install Express and so all method as in nodejs"

--- server.js
+++ server.js
@@ -1,68 +1,63 @@
-const http = require('http');
+const express = require('express');
+// Initialization
+const app = express(); // or calling express server
+
+//here we domt need to specify  { 'Content-Type': 'text/html' }
+
+//Application will now use json format for data
+app.use(express.json()); // Data always transfer in jso format.
 
 const port = 8081;
 
-const toDoList = ["Complete Node Byte", "Play Cricket", "Go to sleep"];
+const toDoList = ["Complete Node Byte", "Cricket", "Go to sleep"];
 
-http.createServer((req, res) => {
-    // response.writeHead(200, { 'Content-Type': 'text/html' }); // this line use send success msg by using 200 
-    // response.write("<h1>Hello All, This is from my server</h1>");
-    // response.write(html);
-    const { method, url } = req;
-    console.log(method, url);
-    // Bydefault browser always send Get
 
-    if (url === "/todos") {
-        if (method === "GET") {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(toDoList.toString());
+//http//localhost:8081/todos
+app.get("/todos", (req, res) => {
+    res.status(200).send(toDoList);
 
-        } else if (method === "POST") {
-            let body = "";
-            req.on('error', () => {
-                console.error(err);
-            }).on('data', (chunk) => {
-                body += chunk;
-                console.log(chunk)
 
-            }).on('end', () => {
-                body = JSON.parse(body);
-                console.log("Body-->", body)
-            })
+});
 
+app.post("/todos", (req, res) => {
+    let newToDoItem = req.body.item;
+    toDoList.push(newToDoItem);
+    console.log(toDoList)
+    res.status(201).send({
+        message: "Task added succefully"
+    });
+
+
+});
+
+app.delete("/todos", (req, res) => {
+    let itemToDelete = req.body.item;
+    toDoList.find((element, index) => {
+        if (element === itemToDelete) {
+            toDoList.splice(index, 1);
         }
+    });
+    console.log(toDoList);
+    res.status(202).send({
+        message: `Deleted item --> ${req.body.item} .`
+    })
 
 
-        else {
-            res.writeHead(404);
-        }
-    } else {
-        res.writeHead(501);
-    }
+});
 
-    res.end();
 
-}).listen(port, () => {
-    console.log(`Nodejs server started on port ${port}`);
-    // console.log(method, url);
+// if u use remainng/other mthd(patch put) which not defined above it give error
+app.all("/todos", (req, res) => {
+    res.status(501).send();
 })
 
-// http://localhost:8081
-
-//we can create a shortcut for typing node server.js in package.json in script tag.
-
-//now we create shortcut which is node start/ npm start for node server.js
-// "start": "node server.js",
-// If we use a default name start then use npm start
-// If u give any name like "runThis" to start --> then u have to say npm run runThis
+//All the other route which not defined above
+app.all('*', (req, res) => {
+    res.status(404).send();
+});
 
 
+app.listen(port, () => {
 
-//---> Now we are going to install nodemeon to refresh ur server/web page again angain.
-//command --> npm i nodemon
-
-//Now we are creating a command in json to restart our server agin and again
-//  "dev": "nodemon server.js"
-
-
-// const html = "<h1>Hello All, This is from my server today</h1><h2> This is just a simple server which i created</h2>"
+    console.log(`Nodejs server started on port ${port}`)
+})
